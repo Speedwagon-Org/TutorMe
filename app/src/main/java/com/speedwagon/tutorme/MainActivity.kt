acb359f7ef29bc11b51575a21435256f331df5d8
Rizky Dendy Nugraha <191111410@students.mikroskil.ac.id>
2022-03-10 14:28:42 +0700

    Initial commit

---
+++ app/src/main/java/com/speedwagon/tutorme/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.speedwagon.tutorme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
