5ba5f97646cd5a15d505b13dd7ee54d1131f9ce7
Brata Hadi Sultan <92165842+bratahs17@users.noreply.github.com>
2022-03-15 18:22:16 +0700

    Added Notification & Explore.

--- app/src/main/java/com/speedwagon/tutorme/Demo.kt
+++ app/src/main/java/com/speedwagon/tutorme/Demo.kt
@@ -1,15 +1,9 @@
 package com.speedwagon.tutorme
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import java.io.File
 
 class Demo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +14,5 @@ class Demo : AppCompatActivity() {
             val MainmenuIntent = Intent(this, home_main::class.java)
             startActivity(MainmenuIntent)
         }
-
     }
 }
--- app/src/main/java/com/speedwagon/tutorme/LoginRegister.kt
+++ app/src/main/java/com/speedwagon/tutorme/LoginRegister.kt
@@ -1,4 +1,16 @@
 package com.speedwagon.tutorme
 
-class LoginRegister {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+
+class LoginRegister : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+
+        val registerFragment = Register()
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().add(R.id.logResLayout,registerFragment).commit()
+    }
 }
